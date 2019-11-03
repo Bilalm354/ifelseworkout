82be6578f1de271e32964dc71c15274f15636cc6
Bilal <bilalm354@gmail.com>
2019-11-03 22:54:50 +0000

    .

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 #if else workout program
+
+#Intro Screen
 print('Hi')
 print('How much do you weigh in kg?')
 weight = input()
-print ('You weigh ' + weight + 'kg')
+print('What would you like to weigh?')
+goalWeight = input()
+
+
+#Start of workout
+print('Hi, do you have any pains today? Yes or No?')
+input() #must be yes or no
+print('How well rested do you feel? from 1-5')
+input() #must be int from 1-5
