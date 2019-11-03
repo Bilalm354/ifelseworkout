9116d3e113c40a405fe3bb903e62738c44acf45e
Bilal <bilalm354@gmail.com>
2019-11-03 23:11:13 +0000

    .

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #if else workout program
 
-#Intro Screen
+#Intro Screen - Saves your goals 
 print('Hi')
 print('How much do you weigh in kg?')
 weight = input()
 print('What would you like to weigh?')
 goalWeight = input()
+#height, BMI, calories, 
+
 
 
 #Start of workout
@@ -13,3 +15,16 @@ print('Hi, do you have any pains today? Yes or No?')
 input() #must be yes or no
 print('How well rested do you feel? from 1-5')
 input() #must be int from 1-5
+
+
+#Tracking/ graphs
+#sleep and steps - from fake apple watch,
+#weight, weights, bodyfat, mood, stress,
+
+#Macro tracker #Micro Tracker
+
+
+
+#feature for personal trainers to see how their clients are doing, make their programs etc. 
+
+
